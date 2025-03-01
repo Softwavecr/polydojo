919f8efa19477fe4a28a64938214ebdd7145bae0
Jairo Guerrero <softwavecr@hotmail.com>
2025-03-01 02:57:11 -0500

    Split line in 2

--- cs/Find.cs
+++ cs/Find.cs
@@ -1,6 +1,6 @@
 public class Find
 {
-    public static List<Hero> FindFunc(List<Hero> heroes, string target) => heroes.FindAll(hero => hero.Name == target);    
+    public static List<Hero> FindFunc(List<Hero> heroes, string target) => 
+        heroes.FindAll(hero => hero.Name == target);    
 }
-
 //{"name":"Hulk","strength":90000}
