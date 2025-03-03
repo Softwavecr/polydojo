adc57e98428edf1da6d1303e4b632a9436de5ff2
Jairo Guerrero <softwavecr@hotmail.com>
2025-03-03 01:29:10 -0500

    Executor

---
+++ go/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	heroes, err := LoadHeroes("data.json")
+	if err != nil {
+		log.Fatalf("Error loading heroes: %v", err)
+	}
+
+	hulk, found := FindHeroByName(heroes, "Hulk")
+	if found {
+		fmt.Println(hulk)
+	} else {
+		fmt.Println("Hulk not found")
+	}
+
+	spiderMan, found := FindHeroByName(heroes, "Spider-Man")
+	if found {
+		fmt.Println(spiderMan)
+	} else {
+		fmt.Println("Spider-Man not found")
+	}
+
+}
