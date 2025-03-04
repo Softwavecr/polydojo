3ef7512bf87c6ddd4bee02dd25a4e4c09921e7de
Jairo Guerrero <softwavecr@hotmail.com>
2025-03-03 23:54:36 -0500

    Draft v1, it works

--- cs/Program.cs
+++ cs/Program.cs
@@ -6,15 +6,33 @@ class Program
     {
         List<Hero> hulk = Find.FindFunc(heroes,"Hulk");
 
-        if (hulk != null) Prin.ter("2- "+JsonSerializer.Serialize(hulk));
-        else Prin.ter("2- Hulk not found.");            
+        var names = Map.MapFunc<Hero, string>(heroes, x => x.Name+"!");
+
+        //if (hulk != null) Prin.ter("2- Find Hulk: "+JsonSerializer.Serialize(hulk));
+        //else Prin.ter("2- Find Hulk: Hulk not found.");
+
+
+        Prin.ter("3- Map.");
+
+        if (names != null) 
+            foreach (var n in names)
+                Prin.ter(n);
+
+        //Prin.ter("\n3- Map complete.\n");
+
+        Prin.ter("3- Map.");
+
+        if (names != null) 
+            foreach (var n in names)
+                Prin.ter(n);
+
     }    
     public static void Main()
     {
         string filePath = "data.json";
         try
         {
-            Prin.ter("1- Print: Hello, J");
+            //Prin.ter("1- Print: Hello, J");
             string jsonString = File.ReadAllText(filePath);
             List<Hero> heroes = JsonSerializer.Deserialize<List<Hero>>(jsonString);
             DoHeroes(heroes);
