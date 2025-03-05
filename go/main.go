0cd528c59e27dcf0945f8d4a17ad42b7307347df
Jairo Guerrero <softwavecr@hotmail.com>
2025-03-04 23:28:41 -0500

    Improve messages

--- go/main.go
+++ go/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	fmt.Println("1- Hello, Heroes")
 	heroes, err := LoadHeroes("data.json")
 	if err != nil {
 		log.Fatalf("Error loading heroes: %v", err)
@@ -13,13 +14,13 @@ func main() {
 
 	hulk, found := FindHeroByName(heroes, "Hulk")
 	if found {
-		fmt.Println(hulk)
+		fmt.Println("2- Find:", hulk)
 	} else {
 		fmt.Println("Hulk not found")
 	}
 
 	filterHeroes := Filter(heroes, Strong)
-	fmt.Println("Filtered Heroes:", filterHeroes)
+	fmt.Println("4- Filter:", filterHeroes)
 }
 
 func Strong(hero Hero) bool {
