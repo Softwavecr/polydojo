9742e88d8a221e799c94f1f192a83575aa9fd94b
Jairo Guerrero <softwavecr@hotmail.com>
2025-03-04 23:50:10 -0500

    Append Func to function names for consistency

--- go/main.go
+++ go/main.go
@@ -6,21 +6,28 @@ import (
 )
 
 func main() {
-	fmt.Println("1- Hello, Heroes")
+	fmt.Println("1- Print:  Hello, Heroes")
 	heroes, err := LoadHeroes("data.json")
 	if err != nil {
 		log.Fatalf("Error loading heroes: %v", err)
 	}
 
-	hulk, found := FindHeroByName(heroes, "Hulk")
+	hulk, found := FindFunc(heroes, "Hulk")
 	if found {
 		fmt.Println("2- Find:", hulk)
 	} else {
 		fmt.Println("Hulk not found")
 	}
 
-	filterHeroes := Filter(heroes, Strong)
+	names := MapFunc(heroes, AppendSymbol)
+	fmt.Println("3- Map:", names)
+
+	filterHeroes := FilterFunc(heroes, Strong)
 	fmt.Println("4- Filter:", filterHeroes)
+}
+
+var AppendSymbol = func(hero Hero) string {
+	return hero.Name + "!"
 }
 
 func Strong(hero Hero) bool {
