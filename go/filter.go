6098695a98b2705a9e67c1ff0c902ac078256c20
Jairo <softwavecr@hotmail.com>
2025-03-04 23:38:52 -0500

    Filter function go (#10)
    * Add Filter go using a predicate parameter

---
+++ go/filter.go
@@ -0,0 +1,11 @@
+package main
+
+func Filter[T any](list []T, predicate func(T) bool) []T {
+	var result []T
+	for _, item := range list {
+		if predicate(item) {
+			result = append(result, item)
+		}
+	}
+	return result
+}
